55c0f2fec860ce55c965964482f37ecb79229e2f
Riccardo Vandra <riccardovandra@Riccardos-Laptop.local>
2023-10-10 16:11:14 +0200

    Working version of researcher bot

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import cloudscraper
+import autogen
 from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
@@ -10,19 +12,33 @@ import openai
 from dotenv import load_dotenv
 import os
 
+#get openai API keys
+load_dotenv()
+config_list = autogen.config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def scrape_website(url:str):
+#Helper function
 
+# def scrape(url:str):
+#     if url.startswith('https://www.linkedin.com'):
+#         scrape_linkedin(url)
+#     else:
+#         scrape_website(url)
+
+def scrape_website(website_url:str):
+
+    # Create a session
+    
+    scraper = cloudscraper.create_scraper()
+    
+    # Set headers
     headers = {
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json',
-    }
-    data = {
-        "url": url
-    }
-
-    data_json = json.dumps(data)
-    response = requests.post("https://chrome.browserless.io/content?token=2db344e9-a08a-4179-8f48-195a2f7ea6ee", headers=headers, data=data_json)
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57"}
+    
+    # Send a GET request to the URL using the session
+    response = scraper.get(website_url, headers=headers)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
@@ -62,7 +78,7 @@ def summarize(content, type):
 
     return output
 
-def scrape_linkedIn(linkedin_url:str):
+def scrape_linkedin(linkedin_url:str):
     json_cache = 'json_cache.json'
     api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
     api_key = os.getenv('PROXYCURL_API')
@@ -100,5 +116,84 @@ def scrape_linkedIn(linkedin_url:str):
     with open(json_cache, 'w') as f:
         json.dump(cached_data, f,  indent=4)
     
-    return new_data['response']
+    output = summarize(new_data['response'],'linkedin')
 
+    return output
+
+
+
+# Create the research function
+
+def research(lead_data:dict):
+    llm_config_research = {
+        "functions" : [
+            {
+                "name": "scrape_website",
+                "description": "scrape the website and look for relevant information",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "website_url": {
+                            "type": "string",
+                            "description": "The Website URL to scrape",
+                        }
+                    },
+                    "required": ["website_url"],
+                },
+            },
+            {
+                "name": "scrape_linkedin",
+                "description": "scrape the website and look for relevant information",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "linkedin_url": {
+                            "type": "string",
+                            "description": "The Linkedin URL to scrape",
+                        }
+                    },
+                    "required": ["linkedin_url"],
+                },
+            }
+        ],
+        "config_list": config_list   
+    }
+
+    outbound_researcher = autogen.AssistantAgent(
+        name="outbound_researcher",
+        system_message="Research about a given prospect, collect as many information as possible, and generate detailed report with every single information that could be used in our outreach efforts; Add TERMINATE to the end of the research report;",
+        llm_config=llm_config_research,
+    )
+
+    user_proxy = autogen.UserProxyAgent(
+        name="User_proxy",
+        code_execution_config={"last_n_messages": 2, "work_dir": "coding"},
+        is_termination_msg=lambda x: x.get("content", "") and x.get(
+            "content", "").rstrip().endswith("TERMINATE"),
+        human_input_mode="TERMINATE",
+        function_map={
+            "scrape_website": scrape_website,
+            "scrape_linkedin": scrape_linkedin
+            }
+    )
+
+    user_proxy.initiate_chat(outbound_researcher, message=f"Research this lead's website and LinkedIn Profile {str(lead_data)}")
+
+    # set the receiver to be researcher, and get a summary of the research report
+    user_proxy.stop_reply_at_receive(outbound_researcher)
+
+    # return the last message the expert received
+    return user_proxy.last_message()["content"]
+
+
+#Generate Lead Data
+lead_data = {
+    'First Name': 'Mulenga',
+    'Company Name': 'Growthcurve',
+    'Website URL' : 'http://growthcurve.co',
+    'LinkedIn URL': 'https://www.linkedin.com/in/mulengaagley'
+}
+
+
+#Run the research function
+research(lead_data)
